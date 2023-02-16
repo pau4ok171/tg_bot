fb2daef256a0d4e899e6059581bb40f88bcbfd19
Pau4ok <pavel_mal99@mail.ru>
2023-02-16 17:04:45 +0300

    calendar refactoring

--- builders/calendar_.py
+++ builders/calendar_.py
@@ -5,7 +5,6 @@ from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 from keyboards.keyboard import Keyboard
 from builders.static import DAYS_OF_WEEK, MONTHS, STEP
 import schemas
-
 import calendar
 from dateutil.relativedelta import relativedelta
 from calendar import monthrange
@@ -31,6 +30,7 @@ class Calendar(Keyboard):
         self.prev_button = '<<'
         self.next_button = '>>'
         self.empty_nav_button = '×'
+        self.empty_date_button = ' '
         self.year_size = 2
         self.month_size = 3
         self.day_size = 7
@@ -39,10 +39,9 @@ class Calendar(Keyboard):
         self.min_date = min_date
         self.ref_date = current_date
 
-    def build(self, params=None, diff=None) -> tuple[schemas.Keyboard | None, date | None]:
-        chosen_date = None
-        kb = None
-        params = params or self._build_params()
+    def build(self, params: dict=None, diff: int=0) -> tuple[schemas.Keyboard | None, date | None]:
+        if not params: params = self._build_params()
+
         step = params['step']
 
         if step == 'YEAR':
@@ -52,24 +51,36 @@ class Calendar(Keyboard):
         elif step == 'DAY':
             buttons = self._build_days(params, diff)
         elif step == 'CHOSEN':
-            chosen_date = self._get_date(int(params['year']), int(params['month']), int(params['day']))
-            return kb, chosen_date
+            chosen_date = date(int(params['year']), int(params['month']), int(params['day']))
+            return None, chosen_date
         else:
             return None, None
 
-        n_buttons = self._build_navigation(params)
+        # Navigation
+        nav_buttons = self._build_navigation(params)
+        # Footer
         f_buttons = self._build_footer()
-        buttons.append(n_buttons)
-        buttons.append(f_buttons)
 
         # Keyboard
         text = f'{self.trans[42]} {STEP[self.lang][step.lower()]}'
-        reply_markup = InlineKeyboardMarkup(buttons)
+        reply_markup = InlineKeyboardMarkup(buttons + nav_buttons+ f_buttons)
 
         kb = self._build_keyboard(text, reply_markup)
-        return kb, chosen_date
+        return kb, None
 
-    def _build_callback(self, action, step, current_date=None, token=None):
+    def _build_callback(self, action, step, current_date=None, token=None) -> str:
+        """
+        :param action: choice, nav_back, nav_middle, nav_next, empty
+        :param step: YEAR, MONTH, DAY, CHOSEN
+        :param current_date: datetime.date
+        :param token:
+        Токен необходим в случае если мы задаем callback для пустых элементов
+        с целью сделать объекты уникальными и избежать удаления этих объектов
+        как дубликатов.
+        :return:
+        Возвращает строку формата: handler&action&step&year&month&day
+        или формата handler&action&step&token если current_date=None.
+        """
         if current_date:
             data = list(map(str, current_date.timetuple()[:3]))
         elif token:
@@ -77,71 +88,77 @@ class Calendar(Keyboard):
         else:
             data = []
 
-        params = [self.handler, action, step] + data
+        callback = '&'.join([self.handler, action, step] + data)
 
-        callback = '&'.join(params)
         return callback
 
-    def _build_years(self, diff) -> list[list[InlineKeyboardButton]]:
+    def _build_years(self, diff: int=0) -> list[list[InlineKeyboardButton]]:
         # Years
         size = self.year_size
-        diff = diff or 0
         self.ref_date += relativedelta(years=diff*4)
-        self.current_date = self.current_date + relativedelta(years=diff*4)
+        self.current_date += relativedelta(years=diff*4)
+        ref_year = self.ref_date.year
 
-        year, month, day = self.ref_date.timetuple()[:3]
-        years = [year if self._valid_date('YEAR', date(year, month, day)) else ' ' for year in range(year-1, year+3)]
+        years = [
+            year if self._valid_date('YEAR', date(year, 1, 1)) else self.empty_date_button
+            for year in range(ref_year-1, ref_year+3)
+        ]
 
         callback = [
             self._build_callback(
-                'choice' if self._valid_date('YEAR', date(year, month, day)) else 'empty',
+                'choice' if self._valid_date('YEAR', date(year, 1, 1)) else 'empty',
                 'YEAR',
-                current_date=self._get_date(year, 1, 1) if self._valid_date('YEAR', date(year, month, day)) else None,
+                date(year, 1, 1),
                 token=util.generate_random_token()
-            ) for year in range(year-1, year+3)
+            ) for year in range(ref_year-1, ref_year+3)
         ]
 
-        buttons = {k: v for k, v in dict(zip(callback, years)).items()}
-        buttons = [[InlineKeyboardButton(text=v, callback_data=k) for k, v in buttons.items()]]
-        buttons = [buttons[0][i:i + size] for i in range(0, len(buttons[0]), size)]
+        buttons = self._build_buttons(
+            text=years,
+            callback=callback,
+            size=size,
+        )
 
         return buttons
 
-    def _get_date(self, year, month, day) -> date:
-        return date(year, month, day)
-
-    def _build_months(self, params, diff) -> list[list[InlineKeyboardButton]]:
+    def _build_months(self, params, diff=0) -> list[list[InlineKeyboardButton]]:
         # Months
         size = self.month_size
-        diff = diff or 0
         year = int(params['year'])
 
-        self.current_date = self._get_date(year, 1, 1) + relativedelta(years=diff)
+        self.current_date = date(year, 1, 1) + relativedelta(years=diff)
 
         year, month, day = self.current_date.timetuple()[:3]
-        months = [MONTHS[self.lang][month-1] if self._valid_date('MONTH', date(year, month, day)) else ' ' for month in range(1,13)]
+
+        months = [
+            MONTHS[self.lang][month-1]
+            if self._valid_date('MONTH', date(year, month, day))
+            else self.empty_date_button
+            for month in range(1,13)
+        ]
 
         callback = [
             self._build_callback(
                 'choice' if self._valid_date('MONTH', date(year, month, day)) else 'empty',
                 'MONTH',
-                current_date=self._get_date(year, month, 1) if self._valid_date('MONTH', date(year, month, day)) else None,
+                date(year, month, 1),
                 token=util.generate_random_token()
             ) for month in range(1,13)
         ]
 
-        buttons = {k: v for k, v in dict(zip(callback, months)).items()}
-        buttons = [[InlineKeyboardButton(text=v, callback_data=k) for k, v in buttons.items()]]
-        buttons = [buttons[0][i:i + size] for i in range(0, len(buttons[0]), size)]
+        buttons = self._build_buttons(
+            text=months,
+            callback=callback,
+            size=size,
+        )
 
         return buttons
 
-    def _build_days(self, params, diff) -> list[list[InlineKeyboardButton]]:
+    def _build_days(self, params, diff=0) -> list[list[InlineKeyboardButton]]:
         # Days
-        diff = diff or 0
         year = int(params['year'])
         month = int(params['month'])
-        self.current_date = self._get_date(year, month, 1) + relativedelta(months=diff)
+        self.current_date = date(year, month, 1) + relativedelta(months=diff)
 
         year, month, day = self.current_date.timetuple()[:3]
 
@@ -150,76 +167,107 @@ class Calendar(Keyboard):
 
         cl = calendar.monthcalendar(year, month)
         if len(cl) == 5:  cl.append([0, 0, 0, 0, 0, 0, 0])
-        days = [day if day != 0 and self._valid_date('DAY', date(year, month, day)) else ' ' for week in cl for day in week]
 
-        callback_of_week = [self._build_callback('empty', 'EMPTY', token=util.generate_random_token()) for i in range(7)]
+        days = [
+            day
+            if day != 0 and self._valid_date('DAY', date(year, month, day))
+            else self.empty_date_button
+            for week in cl for day in week
+        ]
+
+        callback_of_week = [
+            self._build_callback(
+                'empty',
+                'DAY',
+                token=util.generate_random_token()
+            ) for _ in range(7)
+        ]
 
         callback = [
-            self._build_callback(
-                'choice' if day != ' ' else 'empty',
-                'DAY',
-                current_date=self._get_date(year, month, day) if day != ' ' else None,
+            self._build_callback('choice' if day != self.empty_date_button else 'empty', 'DAY',
+                date(year, month, day) if day != self.empty_date_button else None,
                 token=util.generate_random_token()
             ) for day in days
-            ]
+        ]
 
-        days = days_of_week + days
-        callback = callback_of_week + callback
+        buttons = self._build_buttons(
+            text=days_of_week + days,
+            callback=callback_of_week + callback,
+            size=size,
+        )
 
-        buttons = {k: v for k, v in dict(zip(callback, days)).items()}
-        buttons = [[InlineKeyboardButton(text=v, callback_data=k) for k, v in buttons.items()]]
+        return buttons
+
+    @staticmethod
+    def _build_buttons(text: list, callback: list, size: int) -> list[list[InlineKeyboardButton]]:
+        buttons = [[
+            InlineKeyboardButton(
+                text=v,
+                callback_data=k
+            ) for k, v in dict(zip(callback, text)).items()
+        ]]
+
         buttons = [buttons[0][i:i + size] for i in range(0, len(buttons[0]), size)]
 
         return buttons
 
-    def _build_navigation(self, params) -> list[InlineKeyboardButton]:
+    def _build_navigation(self, params) -> list[list[InlineKeyboardButton]]:
         # Navigation
+        size = 3
         nav_middle = None
-        prev_exists = None
-        next_exists = None
+        prev_date = None
+        next_date = None
         step = params['step']
 
         year, month, day = self.current_date.timetuple()[:3]
-        print(self.current_date, self.ref_date)
 
         if step == 'YEAR':
             nav_middle = ' '
-            prev_exists = self._valid_date(step, self.ref_date + relativedelta(years=-2))
-            next_exists = self._valid_date(step, self.ref_date + relativedelta(years=3))
+            prev_date = self.ref_date + relativedelta(years=-2)
+            next_date = self.ref_date + relativedelta(years=3)
         elif step == 'MONTH':
             nav_middle = year
-            prev_exists = self._valid_date(step, self.current_date.replace(month=12) + relativedelta(years=-1))
-            next_exists = self._valid_date(step, self.current_date.replace(month=1) + relativedelta(years=1))
+            prev_date = self.current_date.replace(month=12) + relativedelta(years=-1)
+            next_date = self.current_date.replace(month=1) + relativedelta(years=1)
         elif step == 'DAY':
             nav_middle = f'{MONTHS[self.lang][month - 1]} {year}'
-            prev_exists = self._valid_date(step, self.current_date.replace(day=monthrange(year, month)[1]) + relativedelta(months=-1))
-            next_exists = self._valid_date(step, self.current_date.replace(day=1) + relativedelta(months=1))
+            prev_date = self.current_date.replace(day=monthrange(year, month)[1]) + relativedelta(months=-1)
+            next_date = self.current_date.replace(day=1) + relativedelta(months=1)
 
-        n_buttons = {
-            'nav_back' if prev_exists else 'empty_prev' : '<<' if prev_exists else self.empty_nav_button,
-            'nav_middle': nav_middle,
-            'nav_next' if next_exists else 'empty_next' : '>>' if next_exists else self.empty_nav_button,
-        }
+        prev_exists = self._valid_date(step, prev_date)
+        next_exists = self._valid_date(step, next_date)
 
-        n_buttons = {
-            self._build_callback(
-                k,
-                step,
-                self._get_date(year, month, day)
-            ): v for k, v in n_buttons.items()
-        }
-
-        n_buttons = [
-            InlineKeyboardButton(
-                text=v,
-                callback_data=k
-            ) for k, v in n_buttons.items()
+        text = [
+            self.prev_button if prev_exists else self.empty_nav_button,
+            nav_middle,
+            self.next_button if next_exists else self.empty_nav_button,
         ]
+
+        actions = [
+            'nav_back' if prev_exists else 'empty_prev',
+            'nav_middle',
+            'nav_next' if next_exists else 'empty_next',
+        ]
+
+        callback = [
+            self._build_callback(
+                action,
+                step,
+                date(year, month, day)
+            ) for action in actions
+        ]
+
+        n_buttons = self._build_buttons(
+            text=text,
+            callback=callback,
+            size=size
+        )
 
         return n_buttons
 
-    def _build_footer(self) -> list[InlineKeyboardButton]:
+    def _build_footer(self) -> list[list[InlineKeyboardButton]]:
         # Footer
+        size = 2
         return_button = None
         if self.calendar_id == 1:
             return_button = 10001
@@ -229,9 +277,14 @@ class Calendar(Keyboard):
 
         f_buttons: dict = self.bt.get_buttons_by_id(buttons, lang=self.lang)
 
-        f_buttons: list = [InlineKeyboardButton(
-            text=v,
-            callback_data=k) for k, v in f_buttons.items()]
+        text = list(f_buttons.values())
+        callback = list(f_buttons.keys())
+
+        f_buttons: list = self._build_buttons(
+            text=text,
+            callback=callback,
+            size=size
+        )
 
         return f_buttons
 
@@ -270,10 +323,6 @@ class Calendar(Keyboard):
         action = params['action']
         step = params['step']
 
-        year = int(params['year'])
-        month = int(params['month'])
-        day = int(params['day'])
-
         if action == 'choice':
             params['step'] = NEXT_STEP[step]
             kb, chosen_date = self.build(params)
@@ -287,14 +336,14 @@ class Calendar(Keyboard):
 
         return kb, chosen_date
 
-    def _valid_date(self, step, date):
+    def _valid_date(self, step, d):
         if step == 'YEAR':
-            return self.min_date.year <= date.year <= self.max_date.year
+            return self.min_date.year <= d.year <= self.max_date.year
         elif step == 'MONTH':
             min_date = self.min_date.replace(day=1)
             max_date = self.max_date.replace(day=monthrange(self.max_date.year, self.max_date.month)[1])
-            return min_date <= date <= max_date
+            return min_date <= d <= max_date
         elif step == 'DAY':
-            return self.min_date <= date <= self.max_date
+            return self.min_date <= d <= self.max_date
 
 
